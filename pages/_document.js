23e215412a423cc9169dd6daffbdad11aa8bf030
piafeyaban <niyaziekinci5050@gmail.com>
2020-10-12 08:59:24 +0300

    environment variable

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', ${process.env.GOOGLE_ANALYTICS_KEY});
+                    gtag('config', '${process.env.GOOGLE_ANALYTICS_KEY}');
         `,
     }}
   />
